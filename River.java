d8d2f76198f5b32a78518ac25486b9378905a14c
Byron <byronblocker@gmail.com>
2017-04-24 11:46:14 -0700

    create program to sing chorus of Down in the river

---
+++ River.java
@@ -0,0 +1,9 @@
+public class River {
+  public static void main(String[] args) {
+    System.out.println("As I went down to the river to pray");
+    System.out.println("Studying about that good ol' way");
+    System.out.println("And who shall wear the starry crown");
+    System.out.println("Oh Lord");
+    System.out.println("Show me the way");
+  }
+}
